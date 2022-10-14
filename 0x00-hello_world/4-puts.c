41e9f58575bc24142dec870c7e76d3fe2814d030
yonatanamde <yonatanamdemichael2@gmail.com>
2022-10-13 19:18:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - prints string value
+ *
+ *   * Description - This program a string value
+ *
+ *    * Return: retuns zero
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
+
